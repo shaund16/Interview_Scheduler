acc5871aed7c2b1a972aae22f7a360aa6b61ce19
Shaun <Shaun@Shauns-MacBook-Pro.local>
2021-08-04 16:18:58 -0600

    add interviewers

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,7 +3,7 @@ import DayList from './DayList';
 import 'components/Application.scss';
 import Appointment from 'components/Appointment';
 import axios from 'axios';
-import { getAppointmentsForDay } from 'helpers/selectors';
+import { getAppointmentsForDay, getInterview } from 'helpers/selectors';
 
 
 export default function Application(props) {
@@ -12,17 +12,23 @@ export default function Application(props) {
     days: [],
     appointments: {}
   });
- const dailyAppointments = getAppointmentsForDay(state, state.day);
+ const appointments = getAppointmentsForDay(state, state.day);
 
  const setDay = day => setState({ ...state, day });
 
 
-  const appointments = dailyAppointments.map(appointment => {
-    return (
-      <Appointment 
-      key={appointment.id} {...appointment} />
-    )
-  })
+const schedule = appointments.map((appointment) => {
+  const interview = getInterview(state, appointment.interview);
+
+  return (
+    <Appointment
+      key={appointment.id}
+      id={appointment.id}
+      time={appointment.time}
+      interview={interview}
+    />
+  );
+});
 
  useEffect(() => {
     // axios.get("/api/days").then((response) => setDays(response.data))
@@ -60,7 +66,7 @@ export default function Application(props) {
         />
       </section>
       <section className='schedule'>
-        {appointments}
+        {schedule}
         <Appointment key="last" time="5pm" />
       </section>
     </main>
