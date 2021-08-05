8bfd399a48a037621066531257b474bca02f5998
Shaun <Shaun@Shauns-MacBook-Pro.local>
2021-08-04 19:10:21 -0600

    add functions for hooks

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  function transition(newMode, replace = false) {
+   if (replace) {
+     setHistory((prev) => [...prev.slice(0, prev.length -1), newMode])
+    } else {
+     setHistory((prev) => [...prev, newMode])  
+    }
+  }
+
+  function back() {
+     setHistory((prev) => [...prev.slice(0, prev.length -1)])
+  }
+
+
+  return { mode: history[history.length - 1], transition, back };
+}
