466e293aacf8442d87fce936bb90315cbf6fabeb
Shaun <Shaun@Shauns-MacBook-Pro.local>
2021-08-09 14:59:05 -0600

    update

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import 'components/InterviewerList.scss';
 import InterviewerListItem from './InterviewerListItem';
+import PropTypes from 'prop-types';
 
 const InterviewerList = (props) => {
   const interviewerList = props.interviewers.map((interviewer) => {
@@ -14,6 +15,12 @@ const InterviewerList = (props) => {
       />
     );
   });
+
+  InterviewerList.propTypes = {
+    interviewers: PropTypes.array.isRequired
+  }
+
+
   return (
     <section className='interviewers'>
       <h4 className='interviewers__header text--light'>Interviewer</h4>
